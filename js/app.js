aa975e60e3ae892b5d87d5b7eeb4a6a6667df838
CB230494 <161616911+CB230494@users.noreply.github.com>
2026-06-25 11:20:58 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+const app = document.getElementById("app");
 
+let currentUser = JSON.parse(localStorage.getItem("condoUser")) || null;
+let currentAdmin = JSON.parse(localStorage.getItem("condoAdmin")) || null;
+
+function startApp() {
+  if (currentAdmin) {
+    renderAdmin("dashboard");
+    return;
+  }
+
+  if (currentUser) {
+    renderUsuario("inicio");
+    return;
+  }
+
+  renderLogin();
+}
+
+startApp();
